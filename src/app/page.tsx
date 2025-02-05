ce775916af0ae4dbcdfbae533ee9f96eb8a72078
Cristian Enrique Sánchez Sosa <133435686+Cristian-Sosaa@users.noreply.github.com>
2025-02-04 19:39:50 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,12 +134,10 @@ export default function Page() {
                 Contacto
               </div>
               <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                Get in Touch
+                Ponte en contacto
               </h2>
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                Want to chat? Just shoot me a dm{" "}
-                and I&apos;ll respond whenever I can. I will ignore all
-                soliciting.
+                ¿Quieres chatear? Escríbeme un mensaje privado a cristian.sosa.contact@gmail.com y conversemos.
               </p>
             </div>
           </BlurFade>
